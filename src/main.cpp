98bb57d3ac6d309a030a3357b163263cfe4d91f9
Brandon Vandergriff <95118671+vanderbran@users.noreply.github.com>
2025-10-05 12:46:31 -0500

    added database connection

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,21 @@ Purpose: Creating a C++ project for students to practice merge / rebase on git.
 */
 
 #include<iostream> 
+#include<sqlite3.h>
 
 int main() { 
+    sqlite3* db; 
+
+    int rc = sqlite3_open("todo:name.db", &db); 
+
+    if (rc) { 
+        std::cerr << "Error opening DB: " << sqlite3_errmsg(db) << std::endl; 
+        sqlite3_close(db); 
+        return 1; 
+    }
+
+    std::cout << "Opened database successfully!" << std::endl; 
+    sqlite3_close(db);
 
     return 0; 
 }
